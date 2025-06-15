00609de63c4ddf3223aa42377dc3957d754c1c39
vinayaka <vinayakah47@gmail.com>
2025-06-15 17:00:12 +0530

    Java Servlet DTO

--- Manual/src/service/MarriageapplicationImp.java
+++ Manual/src/service/MarriageapplicationImp.java
@@ -1,6 +1,11 @@
 package service;
 
-public class MarriageapplicationImp implements Marriageapplicationservice{
-    marriage
+import com.xworkz.DTO.MarriageDTO;
 
+public class MarriageapplicationImp implements Marriageapplicationservice{
+    @Override
+    public String MarriageApplicationValidated(MarriageDTO marriageDTO) {
+        return "Failed";
+
+    }
 }
--- Manual/src/service/Marriageapplicationservice.java
+++ Manual/src/service/Marriageapplicationservice.java
@@ -1,4 +1,7 @@
 package service;
 
+import com.xworkz.DTO.MarriageDTO;
+
 public interface Marriageapplicationservice {
+    String MarriageApplicationValidated(MarriageDTO marriageDTO);
 }
