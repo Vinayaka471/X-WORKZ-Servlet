9f76c4b454f16910c24d8c34f1b2607dd0213327
vinayaka <vinayakah47@gmail.com>
2025-08-06 21:08:16 +0530

    Internpe Java

---
+++ Internpe_Java/src/com/xworkz/Internpe/Tailor.java
@@ -0,0 +1,10 @@
+package com.xworkz.Internpe;
+
+public class Tailor {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+
+// Internpe Java From the Scratch Video
